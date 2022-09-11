8302074b838e85caae2b9bbacd64a35c504dbb0e
HappyApplle <112556387+HappyApplle@users.noreply.github.com>
2022-09-11 18:59:48 +0300

    v.3.0

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ window = display.set_mode((500, 500))
 display.set_caption('пинг понг')
 window.fill((1,50,50))
 
+font.init()
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -33,14 +34,16 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < 395:
             self.rect.y += self.speed
 
-ball = GameSprite('ball.png', 200, 300, 100, 100, 10)
+ball = GameSprite('ball.png', 200, 300, 50, 50, 10)
 platform_1 = Player('platform.png', 100, 300, 30, 100, 10)
 platform_2 = Player('platform.png', 400, 300, 30, 100, 10)
 
 speed_x = 3
 speed_y = 3
 
-
+font_1 = font.Font(None, 35)
+lose_left = font_1.render('ЛЕВЫЙ ИГРОК ПРОИГРАЛ!', True, (0,0,0))
+lose_right = font_1.render('ПРАВЫЙ ИГРОК ПРОИГРАЛ!', True, (0,0,0))
 
 clock = time.Clock()
 FPS = 60
@@ -55,12 +58,16 @@ while game:
     platform_2.update_l()
     ball.rect.x += speed_x
     ball.rect.y += speed_y
-    if ball.rect.y < 0 or ball.rect.y > 400:
+    if ball.rect.y < 0 or ball.rect.y > 450:
         speed_y *= -1
     if sprite.collide_rect(ball, platform_1):
         speed_x *= -1
     if sprite.collide_rect(ball, platform_2):
         speed_x *= -1
+    if ball.rect.x > 450:
+        window.blit(lose_right, (50,200))
+    if ball.rect.x < 0:
+        window.blit(lose_left, (50, 200))
     for e in event.get():
         if e.type == QUIT:
             exit()
